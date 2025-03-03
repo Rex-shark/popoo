1322ccde769deb37bc0c535783b0a311dcc53076
rex.liu <rex.liu@tist.com.tw>
2025-03-03 08:50:36 +0800

    spring boot3

---
+++ src/main/java/com/example/popoo/PopooApplication.java
@@ -0,0 +1,13 @@
+package com.example.popoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PopooApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/popoo/PopooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.popoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopooApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
